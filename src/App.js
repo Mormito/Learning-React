4f9a05364cca59d902da2c1ddfe89c1b871ba19f
Guilherme Mormito <guimormis@gmail.com>
2024-08-15 10:38:37 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 
+{/* toda class deve ter o nome de className JSX */}
+
+
 function App() {
   const name = 'Mormito'
 
@@ -7,12 +10,16 @@ function App() {
     return a+b
   }
 
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
       <div>
         <h2>Alterando JSX</h2>
         <p>Olá, {name}</p>
         <p>Soma: {sum(1,2)}</p>      
+        <br />
+        <img src={url} alt="My Image" />
       </div>
     </div>
   );
